6f36b3416c027c593f9601a974a3a315b986c3ab
jlivshots <jlivshots@gmail.com>
2020-11-16 01:25:41 -0600

    Add documentation to virus.h.

--- include/core/virus.h
+++ include/core/virus.h
@@ -2,16 +2,29 @@
 #define EPIDEMIC_SIMULATOR_VIRUS_H
 
 namespace epidemic_simulator {
+/**
+ * Represents a virus and its infection characteristics.
+ */
 class Virus {
  public:
-  Virus(double infectiousness, size_t incubation_period, size_t infectious_period);
+  /**
+   * Instantiates a new Virus object.
+   * @param infectiousness a double between 0-1 where 1 represents 100%
+   * transmission and 0 represents 0% chance of infection.
+   * @param incubation_period the number of days that someone is infectious
+   * without being contagious.
+   * @param infectious_period the number of days that someone is infectious.
+   */
+  Virus(double infectiousness, size_t incubation_period,
+        size_t infectious_period);
   double GetInfectiousness() const;
   size_t GetIncubationPeriod() const;
   size_t GetInfectiousPeriod() const;
+
  private:
   double infectiousness_;
   size_t incubation_period_;
   size_t infectious_period_;
 };
-}
+}  // namespace epidemic_simulator
 #endif  // EPIDEMIC_SIMULATOR_VIRUS_H
