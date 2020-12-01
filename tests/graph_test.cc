671fc61f5954acdfa6089409ac165953f3c34447
jlivshots <jlivshots@gmail.com>
2020-11-30 23:44:08 -0600

    Add tests to graph_test.cc for a graph with 1 person.

--- tests/graph_test.cc
+++ tests/graph_test.cc
@@ -14,7 +14,7 @@ TEST_CASE("No people in simulation") {
     REQUIRE(graph.GetBars().empty());
   }
 
-  SECTION("Graph generates vertical label correctly") {
+  SECTION("Graph generates no vertical labels for 0 days") {
     graph.GenerateVerticalLabels(1);
     const std::vector<LocatedLabel>& labels = graph.GetVerticalLabels();
     REQUIRE(labels.empty());
@@ -32,7 +32,7 @@ TEST_CASE("No people in simulation") {
     REQUIRE(labels.size() == 1);
   }
 
-  SECTION("Horizontal labels are correctly rescaled on 10th day") {
+  SECTION("Horizontal labels are correctly rescaled and located on 10th day") {
     for (size_t i = 0; i < 9; ++i) {
       graph.AddDay(day);
     }
@@ -41,6 +41,80 @@ TEST_CASE("No people in simulation") {
         graph.GetHorizontalLabels();
     REQUIRE(initial_labels.size() == 9);
     graph.AddDay(day);
+    graph.GenerateHorizontalLabels();
+    const std::vector<LocatedLabel>& final_labels = graph.GetHorizontalLabels();
+    REQUIRE(final_labels.size() == 5);
+    for (size_t i = 0; i < final_labels.size(); ++i) {
+      REQUIRE(final_labels[i].first == (i + 1) * 2);
+      REQUIRE(final_labels[i].second.y == 0);
+      REQUIRE(final_labels[i].second.x == 20 * (i + 1));
+    }
+  }
+}
+
+TEST_CASE("1 person in simulation") {
+  epidemic_simulator::Graph graph(100, 100, 1, 1);
+
+  SECTION("Graph Generates no bars") {
+    graph.GenerateBars();
+    REQUIRE(graph.GetBars().empty());
+  }
+
+  SECTION("Graph generates no vertical labels for 0 days") {
+    graph.GenerateVerticalLabels(1);
+    const std::vector<LocatedLabel>& labels = graph.GetVerticalLabels();
+    REQUIRE(labels.empty());
+  }
+
+  SECTION("Graph generates correct vertical labels for 1 day") {
+    std::map<epidemic_simulator::Status, size_t> first_day;
+    first_day.insert(std::make_pair(epidemic_simulator::Status::Vulnerable, 1));
+    first_day.insert(std::make_pair(epidemic_simulator::Status::Infectious, 0));
+    first_day.insert(std::make_pair(epidemic_simulator::Status::Incubating, 0));
+    first_day.insert(std::make_pair(epidemic_simulator::Status::Immune, 0));
+    graph.AddDay(first_day);
+    graph.GenerateVerticalLabels(1);
+    const std::vector<LocatedLabel>& labels = graph.GetVerticalLabels();
+    REQUIRE(labels.size() == 2);
+    REQUIRE(labels[0].first == 0);
+    REQUIRE(labels[0].second.x == 0);
+    REQUIRE(labels[0].second.y == 100);
+    REQUIRE(labels[1].first == 1);
+    REQUIRE(labels[1].second.x == 0);
+    REQUIRE(labels[1].second.y == 0);
+  }
+
+  SECTION("Horizontal labels are generated correctly for 0 days") {
+    graph.GenerateHorizontalLabels();
+    REQUIRE(graph.GetHorizontalLabels().empty());
+  }
+
+  SECTION("Horizontal labels are generated correctly for 1 day") {
+    std::map<epidemic_simulator::Status, size_t> first_day;
+    first_day.insert(std::make_pair(epidemic_simulator::Status::Vulnerable, 0));
+    first_day.insert(std::make_pair(epidemic_simulator::Status::Infectious, 1));
+    first_day.insert(std::make_pair(epidemic_simulator::Status::Incubating, 0));
+    first_day.insert(std::make_pair(epidemic_simulator::Status::Immune, 0));
+    graph.AddDay(first_day);
+    graph.GenerateHorizontalLabels();
+    const std::vector<LocatedLabel>& labels = graph.GetHorizontalLabels();
+    REQUIRE(labels.size() == 1);
+  }
+
+  SECTION("Horizontal labels are correctly rescaled and located on 10th day") {
+    std::map<epidemic_simulator::Status, size_t> first_day;
+    first_day.insert(std::make_pair(epidemic_simulator::Status::Vulnerable, 0));
+    first_day.insert(std::make_pair(epidemic_simulator::Status::Infectious, 0));
+    first_day.insert(std::make_pair(epidemic_simulator::Status::Incubating, 1));
+    first_day.insert(std::make_pair(epidemic_simulator::Status::Immune, 0));
+    for (size_t i = 0; i < 9; ++i) {
+     graph.AddDay(first_day);
+    }
+    graph.GenerateHorizontalLabels();
+    const std::vector<LocatedLabel>& initial_labels =
+        graph.GetHorizontalLabels();
+    REQUIRE(initial_labels.size() == 9);
+    graph.AddDay(first_day);
     graph.GenerateHorizontalLabels();
     const std::vector<LocatedLabel>& final_labels = graph.GetHorizontalLabels();
     REQUIRE(final_labels.size() == 5);
