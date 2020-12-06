0f1be16cf899d9375b650ebf7b10ca798d0b4c0b
jlivshots <jlivshots@gmail.com>
2020-12-05 21:49:27 -0600

    Implement Button constructor in Button.cc.

--- src/core/button.cc
+++ src/core/button.cc
@@ -1,4 +1,9 @@
 #include "core/button.h"
 namespace epidemic_simulator {
 
+Button::Button(const glm::vec2& bottom_right_corner)
+    : is_playing_(false),
+      width_(bottom_right_corner.x),
+      height_(bottom_right_corner.y) {
 }
+}  // namespace epidemic_simulator
