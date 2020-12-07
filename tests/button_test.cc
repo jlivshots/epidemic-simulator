7e36137e9a6a3c79a727a435e29bb168a544c247
jlivshots <jlivshots@gmail.com>
2020-12-07 16:44:04 -0600

    Implement tests for a 5 x 10 button in button_test.cc.

--- tests/button_test.cc
+++ tests/button_test.cc
@@ -51,3 +51,53 @@ TEST_CASE("Button with dimensions 10 X 15") {
     REQUIRE(button.ClickMouse(glm::vec2(8, 15)));
   }
 }
+
+TEST_CASE("Button with dimensions 5 X 10") {
+  epidemic_simulator::Button button(glm::vec2(5, 10));
+  SECTION("Playing status is false by default") {
+    REQUIRE_FALSE(button.IsPlaying());
+  }
+
+  SECTION("Clicking outside of the button does not change playing status") {
+    button.ClickMouse(glm::vec2(654, 465));
+    REQUIRE_FALSE(button.IsPlaying());
+  }
+
+  SECTION("Clicking outside of the button for ClickMouse() returns false") {
+    REQUIRE_FALSE(button.ClickMouse(glm::vec2(654, 465)));
+  }
+
+  SECTION(
+      "Clicking inside the button inverts playing status from false to true") {
+    button.SetPlayingStatus(false);
+    button.ClickMouse(glm::vec2(2, 3));
+    REQUIRE(button.IsPlaying());
+  }
+
+  SECTION(
+      "Clicking inside the button inverts playing status from true to false") {
+    button.SetPlayingStatus(true);
+    button.ClickMouse(glm::vec2(2, 3));
+    REQUIRE_FALSE(button.IsPlaying());
+  }
+
+  SECTION("Clicking inside the button for ClickMouse() returns true") {
+    REQUIRE(button.ClickMouse(glm::vec2(2, 3)));
+  }
+
+  SECTION("Clicking on the button edge inverts playing status from true to false") {
+    button.SetPlayingStatus(true);
+    button.ClickMouse(glm::vec2(2, 10));
+    REQUIRE_FALSE(button.IsPlaying());
+  }
+
+  SECTION("Clicking on the button edge inverts playing status from false to true") {
+    button.SetPlayingStatus(false);
+    button.ClickMouse(glm::vec2(1, 10));
+    REQUIRE(button.IsPlaying());
+  }
+
+  SECTION("Clicking on the button edge for ClickMouse() returns true") {
+    REQUIRE(button.ClickMouse(glm::vec2(0, 2)));
+  }
+}
